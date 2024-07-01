18372b91b02b529bf50f1f129b4ecf8663fa6ab6
Aditya Jain <23110016@iitgn.ac.in>
2024-07-01 18:52:32 +0530

    after adding e

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+
+def get_requirements() -> list[str]:
+
+    requirements_list = []
+
+
+    return requirements_list
+
+setup(
+
+    name = "Sensor",
+    version = "0.0.1",
+    author = "Aditya",
+    author_email = "adityajain1175@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+)
